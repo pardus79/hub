07d31022a35deab383601bee09be9a06437e743e
BtcPins <pardus79@gmail.com>
2024-09-06 12:58:19 -0700

    Update install.sh

--- scripts/linux-x86_64/phoenixd/install.sh
+++ scripts/linux-x86_64/phoenixd/install.sh
@@ -14,15 +14,9 @@ read -p "Absolute install directory path (default: $HOME/albyhub-phoenixd): " US
 
 INSTALL_DIR="${USER_INSTALL_DIR:-$HOME/albyhub-phoenixd}"
 
-echo "Installing phoenixd $PHOENIX_VERSION into $INSTALL_DIR"
-
 mkdir -p $INSTALL_DIR
-mkdir -p "$INSTALL_DIR/phoenixd"
 
 cd $INSTALL_DIR
-
-wget $PHOENIX_URL
-unzip -j phoenix-$PHOENIX_VERSION-linux-x64.zip -d phoenixd
 
 mkdir -p "$INSTALL_DIR/albyhub"
 wget $ALBYHUB_URL
@@ -34,22 +28,14 @@ if [[ $? -ne 0 ]]; then
 fi
 
 rm server-linux-x86_64.tar.bz2
-rm phoenix-$PHOENIX_VERSION-linux-x64.zip
 
 ### Create start scripts
-tee -a $INSTALL_DIR/phoenixd/start.sh > /dev/null << EOF
-#!/bin/bash
-
-echo "Starting phoenixd"
-echo "Make sure to backup your phoenixd data in $INSTALL_DIR/phoenixd/data"
-PHOENIX_DATADIR="$INSTALL_DIR/phoenixd/data" $INSTALL_DIR/phoenixd/phoenixd --agree-to-terms-of-service --http-bind-ip=0.0.0.0
-EOF
 
 tee -a $INSTALL_DIR/albyhub/start.sh > /dev/null << EOF
 #!/bin/bash
 
 echo "Starting Alby Hub"
-phoenix_config_file=$INSTALL_DIR/phoenixd/data/phoenix.conf
+phoenix_config_file=/home/ubuntu/.phoenix/phoenix.conf
 PHOENIXD_AUTHORIZATION=\$(awk -F'=' '/^http-password/{print \$2}' "\$phoenix_config_file")
 WORK_DIR="$INSTALL_DIR/albyhub/data" LN_BACKEND_TYPE=PHOENIX PHOENIXD_ADDRESS="http://localhost:9740" PHOENIXD_AUTHORIZATION=\$PHOENIXD_AUTHORIZATION LOG_EVENTS=true LDK_GOSSIP_SOURCE="" $INSTALL_DIR/albyhub/bin/albyhub
 EOF
@@ -57,16 +43,12 @@ EOF
 tee -a $INSTALL_DIR/start.sh > /dev/null << EOF
 #!/bin/bash
 
-$INSTALL_DIR/phoenixd/start.sh &
-# wait a bit until phoenixd is started
-# especially on the first run to make sure the config is there
-sleep 8
+
 $INSTALL_DIR/albyhub/start.sh &
 echo "Started..."
 EOF
 
 chmod +x $INSTALL_DIR/start.sh
-chmod +x $INSTALL_DIR/phoenixd/start.sh
 chmod +x $INSTALL_DIR/albyhub/start.sh
 
 echo ""
